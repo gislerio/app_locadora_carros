c4e479561e062ab6962433af1b3f19d6195a8bf5
gislerio <gisleriojunior@hotmail.com>
2022-08-10 01:20:30 -0300

    Vue props encaminhando o token e autenticação

--- resources/js/components/Login.vue
+++ resources/js/components/Login.vue
@@ -4,30 +4,57 @@
       <div class="col-md-8">
         <div class="card">
           <div class="card-header">Login Vue</div>
-
           <div class="card-body">
-            <form method="POST" action="">
+            <form method="POST" action="" @submit.prevent="login($event)">
+              <input type="hidden" name="_token" :value="csrf_token" />
               <div class="form-group row">
                 <label for="email" class="col-md-4 col-form-label text-md-right"
                   >Email
                 </label>
 
                 <div class="col-md-6">
-                  <input id="email" type="email" class="form-control" name="email" value="" required autocomplete="email" autofocus/>
+                  <input
+                    id="email"
+                    type="email"
+                    class="form-control"
+                    name="email"
+                    value=""
+                    required
+                    autocomplete="email"
+                    autofocus
+                    v-model="email"
+                  />
                 </div>
               </div>
 
               <div class="form-group row">
-                <label for="password" class="col-md-4 col-form-label text-md-right">Senha</label>
+                <label
+                  for="password"
+                  class="col-md-4 col-form-label text-md-right"
+                  >Senha</label
+                >
                 <div class="col-md-6">
-                  <input id="password" type="password" class="form-control" name="password" required autocomplete="current-password"/>
+                  <input
+                    id="password"
+                    type="password"
+                    class="form-control"
+                    name="password"
+                    required
+                    autocomplete="current-password"
+                    v-model="password"
+                  />
                 </div>
               </div>
 
               <div class="form-group row">
                 <div class="col-md-6 offset-md-4">
                   <div class="form-check">
-                    <input class="form-check-input" type="checkbox" name="remember" id="remember"/>
+                    <input
+                      class="form-check-input"
+                      type="checkbox"
+                      name="remember"
+                      id="remember"
+                    />
 
                     <label class="form-check-label" for="remember">
                       Mentenha-me conectado
@@ -52,4 +79,34 @@
 </template>
 
 <script>
+export default {
+  props: ["csrf_token"],
+  data() {
+    return {
+      email: "",
+      password: "",
+    };
+  },
+  methods: {
+    login(e) {
+      let url = "http://localhost:8000/api/login";
+      let configuracao = {
+        method: "post",
+        body: new URLSearchParams({
+          email: this.email,
+          password: this.password,
+        }),
+      };
+
+      fetch(url, configuracao)
+        .then((response) => response.json())
+        .then((data) => {
+          if (data.token) {
+            document.cookie = "token=" + data.token;
+          }
+          e.target.submit();
+        });
+    },
+  },
+};
 </script>
